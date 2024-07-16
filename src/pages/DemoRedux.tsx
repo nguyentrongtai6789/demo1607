f7cef736abf0565f0d0e6a777275c15b786f097b
trongtai <trongtai.nguyen789@gmail.com>
2024-07-16 15:56:57 +0700

    test project with redux toolkit

---
+++ src/pages/DemoRedux.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../redux/store";
+import { decrement, increment } from "../redux/slice";
+
+export const DemoRedux: FunctionComponent = (props) => {
+  const count = useSelector((state: RootState) => state.counterReducer.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DemoRedux;
