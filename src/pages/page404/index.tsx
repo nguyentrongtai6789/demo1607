b67fae33438ae6d89b65c86ad019ab81b1814f03
trongtai <trongtai.nguyen789@gmail.com>
2024-07-27 15:25:35 +0700

    scss 404

--- src/pages/page404/index.tsx
+++ src/pages/page404/index.tsx
@@ -1,7 +1,19 @@
-import React, { FunctionComponent } from "react";
+import { useTranslation } from "react-i18next";
+import "./styles.scss";
 
-export const index: FunctionComponent = (props) => {
-  return <div>404</div>;
+export default () => {
+  const { t } = useTranslation("dictionnary");
+
+  return (
+    <div className="page-404">
+      <div>
+        <div style={{ fontSize: "100px", fontWeight: "500" }}>404</div>
+        <div
+          style={{ fontSize: "30px", fontWeight: "500", fontStyle: "italic" }}
+        >
+          {t("Not Found")}
+        </div>
+      </div>
+    </div>
+  );
 };
-
-export default index;
