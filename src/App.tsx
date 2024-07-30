1a704ccbfbfee4e1c86628b1a40b23f47805a7e4
trongtai <trongtai.nguyen789@gmail.com>
2024-07-30 15:04:52 +0700

    add LoadingCusom

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Suspense, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { MoonLoader } from "react-spinners";
 import "./App.css";
 import { RootState } from "./redux/store";
 import RoutesOfApp from "./routers/RoutesOfApp";
+import { LoadingCustom } from "./customAntd/LoadingCustom";
 
 function App() {
   const { language, loading } = useSelector((state: RootState) => state.auth);
@@ -20,17 +20,9 @@ function App() {
     changeLanguage(language);
   }, [language]);
 
-  const renderLoading = () => {
-    return (
-      <div className="loading-wrapper">
-        <MoonLoader color="rgb(5 129 105)" speedMultiplier={0.5} size={50} />
-      </div>
-    );
-  };
-
   return (
-    <Suspense fallback={renderLoading()}>
-      {loading && renderLoading()}
+    <Suspense fallback={<LoadingCustom />}>
+      {loading && <LoadingCustom />}
       <BrowserRouter>
         <RoutesOfApp />
       </BrowserRouter>
