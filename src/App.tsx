6df3ed7635d2dec73c2e2e00e211e1d3363f6ec3
trongtai <trongtai.nguyen789@gmail.com>
2024-09-26 10:58:47 +0700

    update tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import "./App.css";
 import { LoadingCustom } from "./customAntd/LoadingCustom";
 import { RootState } from "./redux/store";
 import { router } from "./routers/RoutesOfApp";
